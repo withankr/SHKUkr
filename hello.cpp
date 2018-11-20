547d34cca722404f8504f0efea44857561f0d64a
withankr <43328394+withankr@users.noreply.github.com>
2018-11-20 15:16:56 +0900

    hello.cpp modified on GitHub

--- hello.cpp
+++ hello.cpp
@@ -1,22 +1,12 @@
-
+//For command line git tutorial
+//GitHub modification
 #include <iostream>
 using namespace std;
 
-
-
-
-
 int main(){
-
-
 cout << "hello world" << endl;
-
 cout << "Tell your world" << endl; 
-
 cout << "Tell his world" << endl;
-
-
-
 cout << "Tell your world" << endl; 
 cout << "Tell his world" << endl ; }
 
